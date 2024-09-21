df782770b130bd4813a016eecb6197e44c19ff31
Roni7823 <siy_sar78@yahoo.se>
2024-09-21 02:55:52 +0200

    Lägger till entity scan för att inkludera filmmodeller

--- src/main/java/com/msg/groupprojectmovies/GroupProjectMoviesApplication.java
+++ src/main/java/com/msg/groupprojectmovies/GroupProjectMoviesApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.msg.GroupProjectMovies.Model"})  // Lägg till denna rad
+
+// Commit 2 (SIYAR): Lägger till entity scan för att inkludera filmmodeller.
+// Detta säkerställer att JPA hittar rätt entiteter för databasanrop.
+@EntityScan(basePackages = {"com.msg.GroupProjectMovies.Model"})
+
 public class GroupProjectMoviesApplication {
 
 	public static void main(String[] args) {
